a728725cfd5bffacf05eb0e054b10deb175f3dd7
noriokun4649 <noriokun4649@gmail.com>
2019-06-25 20:50:23 +0900

    空洞の空気

--- src/main/java/mod/torchbowmod/EntityTorch.java
+++ src/main/java/mod/torchbowmod/EntityTorch.java
@@ -147,7 +147,7 @@ public class EntityTorch extends ArrowEntity {
     private boolean isBlockAIR(BlockPos pos) {
         Block getBlock = this.world.getBlockState(pos).getBlock();
         if (getBlock instanceof BushBlock) return true;
-        Block[] a = {Blocks.AIR, Blocks.SNOW};//空気だとみなすブロックリスト
+        Block[] a = {Blocks.CAVE_AIR, Blocks.AIR, Blocks.SNOW};//空気だとみなすブロックリスト
         for (Block traget : a) {
             if (getBlock == traget) return true;
         }
