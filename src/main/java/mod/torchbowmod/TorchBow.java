d16fb50f32ea66b3e4141bcdfb8bfddaeeded061
noriokun4649 <noriokun4649@gmail.com>
2019-07-14 02:52:27 +0900

    Torch Bandolier Support

--- src/main/java/mod/torchbowmod/TorchBow.java
+++ src/main/java/mod/torchbowmod/TorchBow.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.AbstractArrowEntity;
 import net.minecraft.item.*;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.stats.Stats;
 import net.minecraft.util.*;
 import net.minecraft.world.World;
@@ -17,8 +18,10 @@ import static mod.torchbowmod.TorchBowMod.multiTorch;
 import static net.minecraft.item.BowItem.getArrowVelocity;
 
 public class TorchBow extends ShootableItem {
+    private ItemStack torchbinder;
+    private boolean binder;
     public static final Predicate<ItemStack> TORCH = (p_220002_0_) -> {
-        return p_220002_0_.getItem() == Blocks.TORCH.asItem() || p_220002_0_.getItem() == multiTorch;
+        return p_220002_0_.getItem() == Blocks.TORCH.asItem() || p_220002_0_.getItem() == multiTorch || p_220002_0_.getItem() == TorchBowMod.torchbinder ;
     };
 
     public TorchBow(Properties properties) {
@@ -75,10 +78,16 @@ public class TorchBow extends ShootableItem {
                     }
 
                     worldIn.playSound((PlayerEntity) entityLiving, playerentity.posX, playerentity.posY, playerentity.posZ, SoundEvents.ENTITY_ARROW_SHOOT, SoundCategory.PLAYERS, 1.0F, 1.0F / (random.nextFloat() * 0.4F + 1.2F) + f * 0.5F);
-                    if (!flag1 && !playerentity.abilities.isCreativeMode) {
-                        itemstack.shrink(1);
-                        if (itemstack.isEmpty()) {
-                            playerentity.inventory.deleteStack(itemstack);
+                    if (!playerentity.abilities.isCreativeMode){
+                        if (itemstack.getItem() == Blocks.TORCH.asItem() || itemstack.getItem() == multiTorch ) {
+                            itemstack.shrink(1);
+                            if (itemstack.isEmpty()) {
+                                playerentity.inventory.deleteStack(itemstack);
+                            }
+                        } else if (binder) {//TorchBandolierだった場合の処理
+                            int Size = torchbinder.getOrCreateChildTag("TorchBandolier").getInt("Count");//今のアイテムの数取得
+                            int retrun_size = --Size;
+                            torchbinder.getOrCreateChildTag("TorchBandolier").putInt("Count", retrun_size);//TorchBandolierのアイテムの数減少させる。
                         }
                     }
 
@@ -128,16 +137,59 @@ public class TorchBow extends ShootableItem {
 
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
         ItemStack itemstack = playerIn.getHeldItem(handIn);
+        binder = getSilentsMod(playerIn);
         boolean flag = !playerIn.func_213356_f(itemstack).isEmpty();
 
         ActionResult<ItemStack> ret = net.minecraftforge.event.ForgeEventFactory.onArrowNock(itemstack, worldIn, playerIn, handIn, flag);
         if (ret != null) return ret;
 
-        if (!playerIn.abilities.isCreativeMode && !flag) {
+        if (!playerIn.abilities.isCreativeMode && !flag && !binder) {
             return flag ? new ActionResult<>(ActionResultType.PASS, itemstack) : new ActionResult<>(ActionResultType.FAIL, itemstack);
         } else {
             playerIn.setActiveHand(handIn);
             return new ActionResult<>(ActionResultType.SUCCESS, itemstack);
         }
+    }
+    /***
+     * アイテムからアイテムスタック取得。
+     * @param player
+     * @param item
+     * @return　ItemStack
+     */
+    private ItemStack getStack(PlayerEntity player, Item item) {
+        for (int i = 0; i < player.inventory.mainInventory.size(); ++i) {
+            if (player.inventory.mainInventory.get(i) != null && player.inventory.mainInventory.get(i).getItem() == item/*TorchBowMod.StorageBox*/) {
+                ItemStack itemstack = player.inventory.mainInventory.get(i);
+                if (itemstack != null) {//アイテムスタックがからじゃなかったら
+                    if (itemstack.getTag() == null) {//NBTがNullだったら
+                        itemstack.setTag(new CompoundNBT());//新しい空のNBTを書き込む
+                    }
+                }
+                return itemstack;
+            }
+        }
+        ItemStack stack = new ItemStack(Items.BONE);//取得できなかったら適当に骨入れる
+        return stack;
+    }
+    /**
+     * Modのアイテムが有効かどうか、松明が切れてないかどうか
+     * TorchBandolier用処理
+     *
+     * @param player 　プレイヤー
+     * @return Modお問い合わせ
+     */
+    private boolean getSilentsMod(PlayerEntity player) {
+        torchbinder = getStack(player, TorchBowMod.torchbinder);//ItemStack取得
+        boolean mitem = torchbinder.getItem() == TorchBowMod.torchbinder;//正しいかどうかチェック
+        boolean myes = false;//ストレージBoxに入ってるItemのIDチェック用の変数初期化　初期値：無効
+        if (mitem) {
+            int ssize = 0;
+            ssize = torchbinder.getOrCreateChildTag("TorchBandolier").getInt("Count");
+            myes = true;//有効に
+            if (ssize == 0) {
+                myes = false;//無効に
+            }
+        }
+        return myes;
     }
 }
