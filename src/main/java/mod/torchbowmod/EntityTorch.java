c7ccf2c195764eacffd215faf995a84e09764ed7
noriokun4649 <noriokun.official.mail@gmail.com>
2022-06-23 00:07:46 +0900

    Remove import

--- src/main/java/mod/torchbowmod/EntityTorch.java
+++ src/main/java/mod/torchbowmod/EntityTorch.java
@@ -8,7 +8,6 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
